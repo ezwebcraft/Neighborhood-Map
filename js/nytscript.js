8bb722b838814020759525ad6eb49e3a10d3b591
Elvin Ramirez <real8686@gmail.com>
2017-07-27 20:35:01 -0400

    feat: update witg api js

--- js/nytscript.js
+++ js/nytscript.js
@@ -5,6 +5,7 @@ var nytimesUrl = ' '+ theCity +'';
 $.getJSON( "ajax/test.json", function( data ) {
 
   $nytimes-header.text();
+
   var items = [];
   $.each( data, function( key, val ) {
     items.push( "<li id='" + key + "'>" + val + "</li>" );
@@ -14,4 +15,21 @@ $.getJSON( "ajax/test.json", function( data ) {
     "class": "my-new-list",
     html: items.join( "" )
   }).appendTo( "body" );
+});
+
+
+
+// from the nytimes api doc
+// Built by LucyBot. www.lucybot.com
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+  'api-key': "ab8ef0adc1f04b4cafdb7a131d2c34d1"
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
 });
